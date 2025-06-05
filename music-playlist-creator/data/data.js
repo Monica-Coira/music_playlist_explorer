68fc192a189ef3df32463042e857c7a2af16936a
Monica Coira <monicacoira@meta.com>
2025-06-04 21:50:18 -0700

    updated to dynamically insert cards

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,42 @@
+let playlists = [
+	{
+		"playlistID": 1,
+		"playlist_name": "Summer Vibes",
+		"playlist_author": "Emily Wilson",
+		"playlist_art": "assets/img/playlist.png",
+		"songs": "Can't Stop the Feeling, Happy, Uptown Funk, I Gotta Feeling",
+		"likes": 0
+	},
+	{
+		"playlistID": 2,
+		"playlist_name": "Retro Revival",
+		"playlist_author": "Rachel Brown",
+		"playlist_art": "assets/img/playlist.png",
+		"songs": "I Want it that Way, Waterfalls, No Scrubs, Bye Bye Bye, Wannabe",
+		"likes": 0
+	},
+	{
+		"playlistID": 3,
+		"playlist_name": "Workout Motivation",
+		"playlist_author": "Michael Davis",
+		"playlist_art": "assets/img/playlist.png",
+		"songs": "Pumped Up Kicks, Stronger, Roar, All I Do is Win, Unbreakable",
+		"likes": 0
+	},
+	{
+		"playlistID": 4,
+		"playlist_name": "Road Trip",
+		"playlist_author": "Chris Martin",
+		"playlist_art": "assets/img/playlist.png",
+		"songs": "Born to be Wild, Life is a Highway, Sweet Home Alabama, Don't Stop Believin",
+		"likes": 0
+	},
+	{
+		"playlistID": 5,
+		"playlist_name": "Country Roots",
+		"playlist_author": "Jessica Taylor",
+		"playlist_art": "assets/img/playlist.png",
+		"songs": "Friends in Low Places, Jolene, Country Roads",
+		"likes": 0
+	}
+];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,47 @@
+const modal = document.getElementById("playlistModal");
+const span = document.getElementsByClassName("close")[0];
 
+function openModal(playlist) {
+   document.getElementById('playlistName').innerText = playlist.playlist_name;
+   document.getElementById('playlistImage').src = playlist.playlist_art;
+   document.getElementById('playlistCreator').innerText = playlist.playlist_author;
+   document.getElementById('listOfSongs').innerHTML = `<strong>Songs:</strong> ${playlist.songs.join(', ')}`;
+   modal.style.display = "block";
+}
+
+span.onclick = function() {
+   modal.style.display = "none";
+}
+window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+}
+
+const loadPlaylists = () => {
+   console.log("loading playlists");
+   const container = document.querySelector('.playlist-cards');
+   for (const playlist of playlists){
+      const elements = createPlaylistElement(playlist);
+      container.appendChild(elements);
+   }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+   loadPlaylists();
+   const playlistForm = document.querySelector("#playlist-card");
+   playlistForm.addEventListener('click', openModal(playlist));
+});
+
+const createPlaylistElement = (playlist) => {
+   console.log(playlist);
+   const playlistElement = document.createElement('div');
+   playlistElement.id = "playlist-card";
+   playlistElement.innerHTML = `
+      <img src="${playlist.playlist_art}" alt="Song Cover" width="200">
+      <h3>${playlist.playlist_name}</h3>
+      <p>${playlist.playlist_author}</p>
+      <button>Likes (${playlist.likes})</button>
+   `;
+   return playlistElement;
+}
